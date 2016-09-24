342c81c25acd323be9d34793db70ea75b172cc75
nipunasilva <nipunasi@99x.lk>
2016-09-24 20:20:19 +0530

    Revert "Initial commit with files"
    This reverts commit 96c354341c76cd99ea81c483be577a1a84535e92.

--- app/main.ts
+++
@@ -1,9 +0,0 @@
-//Angular - library for using Angular in a web browser
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-//Root Module
-import { AppModule } from './app.module';
-
-const platform = platformBrowserDynamic();
-
-platform.bootstrapModule(AppModule);
